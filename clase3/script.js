d91b796684c09123852b860f6b76fe013fb4ca10
LukePernigotti <luke.pernigotti.90@gmail.com>
2019-08-02 02:18:15 -0300

    Ejercicio miercoles 31-7 - clase 3 - v1.1

--- clase3/script.js
+++ clase3/script.js
@@ -1,32 +1,38 @@
 let a = document.body.children[0];
 let f = document.createDocumentFragment();
+let flag = false;
 
 a.addEventListener("click", e => {
     e.preventDefault();
 
-    let aceptar = document.createElement("button");
-    aceptar.innerText = "Aceptar";
-    aceptar.id = "aceptar";
-    f.appendChild(aceptar);
+    if (!flag) {
+        let aceptar = document.createElement("button");
+        aceptar.innerText = "Aceptar";
+        aceptar.id = "aceptar";
+        f.appendChild(aceptar);
 
-    let cancelar = document.createElement("button");
-    cancelar.innerText = "Cancelar";
-    cancelar.id = "cancelar";
-    f.appendChild(cancelar);
+        let cancelar = document.createElement("button");
+        cancelar.innerText = "Cancelar";
+        cancelar.id = "cancelar";
+        f.appendChild(cancelar);
 
-    let p = document.createElement("p");
-    p.innerText = "Esta seguro que quiere abandonar la página";
-    f.appendChild(p);
+        let p = document.createElement("p");
+        p.innerText = "Esta seguro que quiere abandonar la página";
+        f.appendChild(p);
 
-    let div = document.createElement("div");
-    div.appendChild(f);
-    document.body.appendChild(div);
+        let div = document.createElement("div");
+        div.appendChild(f);
+        document.body.appendChild(div);
+    }
+
+    flag = true;
 })
 
 document.body.addEventListener("click", e => {
     if (e.target.id == "aceptar") {
-        location = "https://google.com";
+        location = a.href;
     } else if (e.target.id == "cancelar") {
         document.body.removeChild(cancelar.parentNode);
+        flag = false;
     }
 })
