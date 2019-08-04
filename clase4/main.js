b6cf070507481d40915f67d9d707838dc3f2658a
LukePernigotti <luke.pernigotti.90@gmail.com>
2019-08-04 18:41:01 -0300

    Ejercicio 1/8 - clase 4

---
+++ clase4/main.js
@@ -0,0 +1,17 @@
+let links = document.getElementsByClassName("links");
+let main = document.getElementsByTagName("main")[0];
+
+const ajaxMain = e => {
+    e.preventDefault();
+
+    let xhr = new XMLHttpRequest;
+    xhr.open("get", `${e.target.href}`);
+    xhr.addEventListener("load", () => {
+        if (xhr.status == 200) {
+            main.innerHTML = xhr.response;
+        }
+    })
+    xhr.send();
+}
+
+[...links].forEach(link => { link.onclick = ajaxMain; });
